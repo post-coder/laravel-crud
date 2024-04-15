b813f071cea423430abb82f00747bee563d70c2a
post-coder <gabrielspanu.dev@gmail.com>
2024-04-15 12:17:54 +0200

    validazione corretta in create form

--- resources/views/pastas/create.blade.php
+++ resources/views/pastas/create.blade.php
@@ -14,64 +14,64 @@
         <div class="mb-3">
           <label for="title" class="form-label">Nome</label>
           <input type="text" class="form-control @error('title') is-invalid @enderror" id="title" name="title" value="{{old('title')}}">
+          @error('title')
+          <div class="invalid-feedback">
+              {{$message}}
+          </div>
+          @enderror
         </div>
 
-        @error('title')
-        <div class="alert alert-danger">
-            {{$message}}
-        </div>
-        @enderror
 
         <div class="mb-3">
             <label for="description" class="form-label">Descrizione</label>
             <textarea type="text" class="form-control @error('description') is-invalid @enderror" id="description" name="description">{{ old('description') }}</textarea>
+            @error('description')
+            <div class="invalid-feedback">
+                {{$message}}
+            </div>
+            @enderror
         </div>
-        @error('description')
-        <div class="alert alert-danger">
-            {{$message}}
-        </div>
-        @enderror
-
+        
         <div class="mb-3">
             <label for="type" class="form-label">Tipologia</label>
             <input type="text" class="form-control @error('type') is-invalid @enderror"  id="type" name="type" value="{{old('type')}}">
+            @error('type')
+            <div class="invalid-feedback">
+                {{$message}}
+            </div>
+            @enderror
         </div>
-        @error('type')
-        <div class="alert alert-danger">
-            {{$message}}
-        </div>
-        @enderror
-
+        
         <div class="mb-3">
             <label for="src" class="form-label">Src immagine</label>
             <input type="text" class="form-control @error('src') is-invalid @enderror" id="src" name="src" value="{{old('src')}}">
+            @error('src')
+            <div class="invalid-feedback">
+                {{$message}}
+            </div>
+            @enderror
         </div>
-        @error('src')
-        <div class="alert alert-danger">
-            {{$message}}
-        </div>
-        @enderror
-
+        
         <div class="mb-3">
             <label for="cooking-time" class="form-label">Tempo di cottura</label>
             <input type="text" class="form-control @error('cooking-time') is-invalid @enderror" id="cooking-time" name="cooking-time" value="{{old('cooking-time')}}">
+            @error('cooking-time')
+            <div class="invalid-feedback">
+                {{$message}}
+            </div>
+            @enderror
         </div>
-        @error('cooking-time')
-        <div class="alert alert-danger">
-            {{$message}}
-        </div>
-        @enderror
-
+        
         <div class="mb-3">
             <label for="weight" class="form-label">Peso</label>
             <input type="text" class="form-control @error('weight') is-invalid @enderror" id="weight" name="weight" value="{{old('weight')}}">
+            @error('weight')
+            <div class="invalid-feedback">
+                {{$message}}
+            </div>
+            @enderror
         </div>
-        @error('weight')
-        <div class="alert alert-danger">
-            {{$message}}
-        </div>
-        @enderror
-
+        
 
         <button type="submit" class="btn btn-primary">Salva</button>
 
