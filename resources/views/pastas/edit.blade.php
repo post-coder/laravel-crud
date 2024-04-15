5b1c5fd51d96abd80510e0dd8cd5114ac736ac3d
post-coder <gabrielspanu.dev@gmail.com>
2024-04-15 12:20:19 +0200

    output errori in update form

--- resources/views/pastas/edit.blade.php
+++ resources/views/pastas/edit.blade.php
@@ -13,43 +13,64 @@
 
         <div class="mb-3">
           <label for="title" class="form-label">Nome</label>
-          <input type="text" class="form-control" id="title" name="title" value="{{$pasta->title}}" required>
+          <input type="text" class="form-control @error('title') is-invalid @enderror" id="title" name="title" value="{{$pasta->title}}" required>
+          @error('title')
+          <div class="invalid-feedback">
+              {{$message}}
+          </div>
+          @enderror
         </div>
 
         <div class="mb-3">
             <label for="description" class="form-label">Descrizione</label>
-            <textarea type="text" class="form-control" id="description" name="description" rows="4">{{$pasta->description}}</textarea>
+            <textarea type="text" class="form-control @error('description') is-invalid @enderror" id="description" name="description" rows="4">{{$pasta->description}}</textarea>
+            @error('description')
+            <div class="invalid-feedback">
+                {{$message}}
+            </div>
+            @enderror
         </div>
 
         <div class="mb-3">
             <label for="type" class="form-label">Tipologia</label>
-            <input type="text" class="form-control" id="type" name="type" value="{{$pasta->type}}">
+            <input type="text" class="form-control @error('type') is-invalid @enderror" id="type" name="type" value="{{$pasta->type}}">
+             @error('type')
+            <div class="invalid-feedback">
+                {{$message}}
+            </div>
+            @enderror
         </div>
 
         <div class="mb-3">
             <label for="src" class="form-label">Src immagine</label>
-            <input type="text" class="form-control" id="src" name="src" value="{{$pasta->src}}">
+            <input type="text" class="form-control @error('src') is-invalid @enderror" id="src" name="src" value="{{$pasta->src}}">
+            @error('src')
+            <div class="invalid-feedback">
+                {{$message}}
+            </div>
+            @enderror
         </div>
 
         <div class="mb-3">
             <label for="cooking-time" class="form-label">Tempo di cottura</label>
-            <input type="text" class="form-control" id="cooking-time" name="cooking-time" value="{{$pasta->cooking_time}}">
+            <input type="text" class="form-control @error('cooking-time') is-invalid @enderror" id="cooking-time" name="cooking-time" value="{{$pasta->cooking_time}}">
+            @error('cooking-time')
+            <div class="invalid-feedback">
+                {{$message}}
+            </div>
+            @enderror
         </div>
 
         <div class="mb-3">
             <label for="weight" class="form-label">Peso</label>
-            <input type="text" class="form-control" id="weight" name="weight" value="{{$pasta->weight}}">
+            <input type="text" class="form-control @error('weight') is-invalid @enderror" id="weight" name="weight" value="{{$pasta->weight}}">
+            @error('weight')
+            <div class="invalid-feedback">
+                {{$message}}
+            </div>
+            @enderror
         </div>
 
-        @if($errors->any())
-        <div class="alert alert-danger">
-            <ul>
-                @foreach($errors->all() as $error)
-                <li>{{$error}}</li>
-                @endforeach
-            </ul>
-        </div>
-        @endif
 
         <button type="submit" class="btn btn-primary">Salva</button>
 
