09f74f6ee819ee2df6751896ed361c85fbbfbfbe
post-coder <gabrielspanu.dev@gmail.com>
2024-04-15 12:15:44 +0200

    create form finito

--- resources/views/pastas/create.blade.php
+++ resources/views/pastas/create.blade.php
@@ -13,43 +13,65 @@
 
         <div class="mb-3">
           <label for="title" class="form-label">Nome</label>
-          <input type="text" class="form-control" id="title" name="title" value="{{old('title')}}">
+          <input type="text" class="form-control @error('title') is-invalid @enderror" id="title" name="title" value="{{old('title')}}">
         </div>
+
+        @error('title')
+        <div class="alert alert-danger">
+            {{$message}}
+        </div>
+        @enderror
 
         <div class="mb-3">
             <label for="description" class="form-label">Descrizione</label>
-            <textarea type="text" class="form-control" id="description" name="description">{{ old('description') }}</textarea>
+            <textarea type="text" class="form-control @error('description') is-invalid @enderror" id="description" name="description">{{ old('description') }}</textarea>
         </div>
+        @error('description')
+        <div class="alert alert-danger">
+            {{$message}}
+        </div>
+        @enderror
 
         <div class="mb-3">
             <label for="type" class="form-label">Tipologia</label>
-            <input type="text" class="form-control" id="type" name="type" value="{{old('type')}}">
+            <input type="text" class="form-control @error('type') is-invalid @enderror"  id="type" name="type" value="{{old('type')}}">
         </div>
+        @error('type')
+        <div class="alert alert-danger">
+            {{$message}}
+        </div>
+        @enderror
 
         <div class="mb-3">
             <label for="src" class="form-label">Src immagine</label>
-            <input type="text" class="form-control" id="src" name="src" value="{{old('src')}}">
+            <input type="text" class="form-control @error('src') is-invalid @enderror" id="src" name="src" value="{{old('src')}}">
         </div>
+        @error('src')
+        <div class="alert alert-danger">
+            {{$message}}
+        </div>
+        @enderror
 
         <div class="mb-3">
             <label for="cooking-time" class="form-label">Tempo di cottura</label>
-            <input type="text" class="form-control" id="cooking-time" name="cooking-time" value="{{old('cooking-time')}}">
+            <input type="text" class="form-control @error('cooking-time') is-invalid @enderror" id="cooking-time" name="cooking-time" value="{{old('cooking-time')}}">
         </div>
+        @error('cooking-time')
+        <div class="alert alert-danger">
+            {{$message}}
+        </div>
+        @enderror
 
         <div class="mb-3">
             <label for="weight" class="form-label">Peso</label>
-            <input type="text" class="form-control" id="weight" name="weight" value="{{old('weight')}}">
+            <input type="text" class="form-control @error('weight') is-invalid @enderror" id="weight" name="weight" value="{{old('weight')}}">
         </div>
-
-        @if($errors->any())
+        @error('weight')
         <div class="alert alert-danger">
-            <ul>
-                @foreach($errors->all() as $error)
-                <li>{{$error}}</li>
-                @endforeach
-            </ul>
+            {{$message}}
         </div>
-        @endif
+        @enderror
+
 
         <button type="submit" class="btn btn-primary">Salva</button>
 
