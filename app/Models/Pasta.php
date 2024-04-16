e9ce7609aaf03645c1425cae20da15a11003354f
post-coder <gabrielspanu.dev@gmail.com>
2024-04-16 10:34:19 +0200

    soft delete

--- app/Models/Pasta.php
+++ app/Models/Pasta.php
@@ -4,10 +4,12 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Pasta extends Model
 {
     use HasFactory;
 
-    
+    // Laravel ci fornisce degli strumenti e delle proprietà che ci permettono di gestire la soft delete, dobbiamo solo farle ereditare dal nostro model
+    use SoftDeletes;
 }
