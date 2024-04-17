37da25e406eabba0239b60bb529414849f0a2e0c
post-coder <gabrielspanu.dev@gmail.com>
2024-04-17 10:41:34 +0200

    feat: subtitle in homepage

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,6 +4,8 @@
 <div class="container py-5">
     <h1 class="mb-5">La casa della pasta: la Casta.</h1>
 
+    <h2 class="mb-4">Vuoi esplorare la nostra offerta?</h2>
+
     <a href="{{route('pastas.index')}}" class="btn btn-outline-primary">Guarda tutti i nostri tipi di pasta</a>
 </div>
 @endsection
